5b0636df7735b5865905f9b34db9d35fd927119e
YAHYA NAMIR <132499945+YahyaNamir@users.noreply.github.com>
2024-07-24 10:51:25 +0100

    Add one player

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ const App = () => {
     { id: '6', name: 'IDRIS' },
     { id: '7', name: 'IDRIS-1' },
     { id: '8', name: 'IDRIS-2' },
+    { id: '9', name: 'IDRIS-3' },
   ]);
 
   const [absentPlayers, setAbsentPlayers] = useState([
